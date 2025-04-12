affa9a5c566f5feef73eefdabe7b3ec282c67714
aslogd <gerarddelcastillo@gmail.com>
2025-04-12 23:46:53 +0200

    export color util

--- mod.ts
+++ mod.ts
@@ -2,3 +2,4 @@ import { SketchyBar as SketchyClass } from "./SketchyBar.ts";
 import { run } from "./run.ts";
 export const SketchyBar: SketchyClass = new SketchyClass(run);
 export * from "./types.ts";
+export * from "./color.ts";
